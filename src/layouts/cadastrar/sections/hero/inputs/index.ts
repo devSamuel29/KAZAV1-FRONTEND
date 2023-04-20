6eef0bfa9b61474f647a5b3b8ea705322bc91813
unknown <samueldflima@protonmail.com>
2023-04-20 17:15:09 -0300

    Bug git

---
+++ src/layouts/cadastrar/sections/hero/inputs/index.ts
@@ -0,0 +1,4 @@
+export { FirstLastnameFields } from './firstname-lastname'
+export { CpfPhoneFields } from './cpf-phone'
+export { EmailFields } from './emails'
+export { PasswordsFields } from './passwords'
